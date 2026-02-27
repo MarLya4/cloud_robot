8d48a451aa29a376d9dc91d8f78a83fa3db3a8cc
Lya4444 <marthalopezresearch@gmail.com>
2026-02-27 15:00:16 -0600

    Create setup.py

---
+++ cloud_robot/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+from glob import glob
+package_name = 'cloud_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/robot', glob('robot/*')),
+        ('share/' + package_name + '/urdf',
+        glob('urdf/*.xacro') +
+        glob('urdf/*.gazebo') +
+        glob('urdf/*.trans') +
+        glob('urdf/*.urdf')),
+
+        ('share/' + package_name + '/urdf/meshes',glob('urdf/meshes/*')),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/controllers', glob('controllers/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cloud',
+    maintainer_email='cloud@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ]
+    },
+)
