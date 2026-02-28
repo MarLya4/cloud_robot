b3871ef3f2c2ab9cb59d450787b7239384bfbd69
Lya4444 <marthalopezresearch@gmail.com>
2026-02-28 02:42:05 -0600

    Refactor launch file to use package share directory

--- cloud_robot/launch/bringup_launch.py
+++ cloud_robot/launch/bringup_launch.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 import os
 import xacro
 
 def generate_launch_description():
-    # Usar la ruta directa en src
-    pkg_dir = os.path.join(os.environ['HOME'], 'ros2_ws_raspi', 'src', 'cloud_robot')
-    xacro_file = os.path.join(pkg_dir, 'urdf', 'URDF_DIRECCION_X.xacro')
-    
-    # Procesar Xacro
+    cloud_robot_dir = get_package_share_directory('cloud_robot')
+
+    # Procesar XACRO
+    xacro_file = os.path.join(cloud_robot_dir, 'urdf', 'URDF_DIRECCION_X.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_description = {'robot_description': robot_description_config.toxml()}
 
@@ -21,24 +21,55 @@ def generate_launch_description():
         parameters=[robot_description]
     )
 
-    # Nodo ros2_control
+    # Nodo ros2_control con tu YAML de hardware
     ros2_control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
-        parameters=[robot_description, os.path.join(pkg_dir, 'config', 'cloud_hardware.yaml')],
+        parameters=[
+            robot_description,
+            os.path.join(cloud_robot_dir, 'config', 'cloud_hardware.yaml')
+        ],
         output='screen'
     )
 
-    # Spawner de servo_controller
+    # Spawners de controladores
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '--controller-manager', '/controller_manager',
+            '--timeout', '10'
+        ],
+        output='screen'
+    )
+
+    diff_drive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'diff_drive_controller',
+            '--controller-manager', '/controller_manager',
+            '--timeout', '10'
+        ],
+        output='screen'
+    )
+
     servo_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['servo_controller', '--controller-manager', '/controller_manager'],
+        arguments=[
+            'servo_controller',
+            '--controller-manager', '/controller_manager',
+            '--timeout', '10'
+        ],
         output='screen'
     )
 
     return LaunchDescription([
         robot_state_publisher_node,
         ros2_control_node,
+        joint_state_broadcaster_spawner,
+        #diff_drive_controller_spawner,
         servo_controller_spawner
     ])
