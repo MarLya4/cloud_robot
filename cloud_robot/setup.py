2edfe15dac0c64ebddad8a173f24e027f76cd763
Lya4444 <marthalopezresearch@gmail.com>
2026-02-28 02:41:02 -0600

    Refactor setup.py to remove unused function and clean up data_files

--- cloud_robot/setup.py
+++ cloud_robot/setup.py
@@ -1,30 +1,31 @@
 from setuptools import find_packages, setup
 from glob import glob
-import os
-
 package_name = 'cloud_robot'
-
-# Función para obtener todos los archivos dentro de un directorio
-def get_files_recursive(path):
-    files = []
-    for root, _, filenames in os.walk(path):
-        for f in filenames:
-            files.append(os.path.join(root, f))
-    return files
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # robot folder (si tienes algo ahí)
         ('share/' + package_name + '/robot', glob('robot/*')),
-        ('share/' + package_name + '/urdf', get_files_recursive('urdf')),
-        ('share/' + package_name + '/urdf/meshes', get_files_recursive('urdf/meshes')),
+        # URDF/XACRO y archivos relacionados
+        ('share/' + package_name + '/urdf', [
+            'urdf/URDF_DIRECCION_X.xacro',
+            'urdf/materials.xacro',
+            'urdf/ros2_control.xacro',
+            'urdf/URDF_DIRECCION_X.gazebo',
+            'urdf/URDF_DIRECCION_X.trans',
+        ]),
+        # meshes (subcarpetas incluidas)
+        ('share/' + package_name + '/urdf/meshes', glob('urdf/meshes/**/*', recursive=True)),
+        # launch files
         ('share/' + package_name + '/launch', glob('launch/*.py')),
+        # config files
         ('share/' + package_name + '/config', glob('config/*')),
-        ('share/' + package_name + '/controllers', glob('controllers/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -33,9 +34,12 @@ setup(
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
-        'test': ['pytest'],
+        'test': [
+            'pytest',
+        ],
     },
     entry_points={
-        'console_scripts': []
+        'console_scripts': [
+        ]
     },
 )
